556b2cbe2c506778498815c835cb0741dd262bde
kummarimanisha <108928977+kummarimanisha@users.noreply.github.com>
2022-07-11 17:04:31 +0530

    Add files via upload

---
+++ SpringApplication-1/src/main/java/com/example/demo/SpringApplication1Application.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringApplication1Application {
+
+	public static void main(String[] args) {
+	
+		
+
+
+SpringApplication.run(SpringApplication1Application.class, args);
+		}
+}
